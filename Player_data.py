153f71651d84df9641a41e94d283a52c8d57485c
aivanits <aivanitskiy2505@gmail.com>
2020-03-16 13:17:46 -0400

    player home data parser, git ignore

--- Player_data.py
+++ Player_data.py
@@ -3,30 +3,40 @@ from os import listdir
 # listdir contains the filenames of all the `.yml` files
 # print(listdir())
 
+
+def check(datafile, str):
+    found = False
+    for line in datafile:
+        if str in line:
+            found = True
+    return found
+
+
+
 # open the output file
 with open( 'SUMMARY.csv', 'w') as out:
     # print output file header
-    print( 'name,x,z,', file=out)
+    print( 'x,z',file=out)
 
     # open warpfiles
-    for warpfile in listdir():
-        with open( warpfile , 'r') as f: #keeps file open 
+    
+    for userdata in listdir():
+        with open( userdata, 'r') as f: #keeps file open 
             linenum = 0
             for str_line in f: # reads every line
-                # TODO: from here on out, this needs to be edited
-                str_line = str_line.split()
-                str_line = str_line[1]
-                name = ''
-                x_coord = ''
-                z_coord = ''
-                linenum = linenum + 1 
-                if linenum == 26:
-                    x_coord = str_line
-                elif linenum == 28:
-                    z_coord = str_line
-                elif linenum == 1:
-                    name = str_line 
-            
+
+                if check(f, 'homes') == True  :
+                
+                    str_line = str_line.split()
+                    str_line = str_line[1]
+                    x_coord = ''
+                    z_coord = ''
+                    linenum = linenum + 1 
+                    if linenum == 26:
+                        x_coord = str_line
+                    elif linenum == 28:
+                        z_coord = str_line
+                
                 
                 # # split each line into an `temp_N`, and `temp_P`
                 # str_N, str_P = str_line.split()
@@ -35,11 +45,11 @@ with open( 'SUMMARY.csv', 'w') as out:
                 # # print the result to the console
 
                 outstring = (
-                    '%s,%s,%s'
+                    '%s,%s'
                     %
-                    ( name, x_coord, z_coord,)
+                    (x_coord, z_coord,)
                 )
-                print( outstring, file=out)
+                print( outstring,file=out)
 
 
 #open folder 
